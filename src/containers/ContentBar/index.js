bb70d2fe761bf03db9294ca7f19695f1553a8aef
Vadim Komolov <vkomolov.ua@gmail.com>
2023-04-12 19:50:03 +0300

    to animate ContentBar on componentDidUpdate

--- src/containers/ContentBar/index.js
+++ src/containers/ContentBar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component, createRef } from "react";
 import * as PropTypes from "prop-types";
 
 import "./ContentBar.scss";
@@ -10,33 +10,65 @@ import { v4 } from "uuid";
  * @param { Object } contentData
  * @constructor
  */
-export default function ContentBar({ contentData }) {
+export default class ContentBar extends Component {
+    constructor(props) {
+        super(props);
 
-    log(contentData, "data inside ContentBar");
+        this.ref = createRef();
+    }
 
-    const { filterActive, data } = contentData;
-    const { title, details } = data;
+    render() {
+        const { contentData } = this.props;
 
+        //log(contentData, "data inside ContentBar");
 
-    const specClassName = filterActive === "personal" ? "personal-spec" : null;
-    let contentArr = details.map(data => {
-        const classAux = specClassName && ("contentWrapper" + " " + specClassName) || "contentWrapper";
+        const { filterActive, data } = contentData;
+        /**
+         * avoiding additional state, this.filterActiveCurrent is used for componentDidUpdate
+         * in order to check if filterActive has changed. If true then to set animation fadeIn
+         */
+        this.filterActiveCurrent = filterActive;
+        const { title, details } = data;
+        const specClassName = filterActive === "personal" ? "personal-spec" : null;
+        const contentArr = details.map(data => {
+            const classAux = specClassName && ("contentWrapper" + " " + specClassName) || "contentWrapper";
 
-        return <ContentItem
-            { ...{ data } }
-            classAux={ classAux }
-            key={ v4() }
-        />
-    });
+            return <ContentItem
+                { ...{ data } }
+                classAux={ classAux }
+                key={ v4() }
+            />
+        });
 
-    return (
-        <main className="contentBar">
-            <h2 className="heading">
-                { title }
-            </h2>
-            { contentArr }
-        </main>
-    );
+        return (
+            <main
+                className="contentBar fade-in"
+                ref={ this.ref }
+            >
+                <h2 className="heading" >
+                    { title }
+                </h2>
+                { contentArr }
+            </main>
+        );
+    }
+
+    /**
+     * @param { Object } contentData from 'prevProps'
+     */
+    componentDidUpdate({ contentData }) {
+        log("componentDidUpdate...");
+
+        const contentBarHtml = this.ref.current;
+        const prevFilterActive = contentData.filterActive;
+        if (this.filterActiveCurrent !== prevFilterActive) {
+            contentBarHtml.classList.toggle("fade-in");
+
+            setTimeout(() => {
+                contentBarHtml.classList.toggle("fade-in");
+            }, 200);
+        }
+    }
 }
 
 ContentBar.propTypes = {
