b4b5913034dd20c7ca0e7085a7df647904339550
Vadim Komolov <vkomolov.ua@gmail.com>
2023-05-03 19:00:59 +0300

    to make ScrollingText functional

--- src/containers/ScrollingText/index.js
+++ src/containers/ScrollingText/index.js
@@ -1,86 +1,33 @@
-import React, { Component, createRef } from "react";
+import React, { useEffect, useRef } from "react";
 import "./ScrollingText.scss";
+import { initScrollingText } from "../../utils/services/userService";
 import * as PropTypes from "prop-types";
 
-export default class ScrollingText extends Component {
-    constructor(props) {
-        super(props);
-
-        this.refContainer = createRef();
-    }
-
-    _initScrollingText(elem, duration, isInfinite = false) {
-        let startTime = null;
-        const textWrapper = elem.parentElement;
-        const distanceGap = 10;
-        const initialLeft = textWrapper.offsetWidth + distanceGap + "px";
-
-        elem.style.left = initialLeft;
-
-        const initialLeftNum = parseInt(elem.style.left);
-        const distance = elem.offsetWidth + textWrapper.offsetWidth + distanceGap;
-
-        const refreshTextPos = () => {
-            elem.remove();
-            elem.style.left = initialLeft;
-            textWrapper.append(elem);
-        };
+export default function ScrollingText({data}) {
+    const refContainer = useRef(null);
+    const { text, duration, isFinite } = data;
 
 
-        requestAnimationFrame(function measure(time) {
-            if (!startTime) {
-                startTime = time;
-            }
-            //getting the percentage of time, passed from startTime...
-            let progress = (time - startTime)/duration;   //from 0 to 1
-            if (progress > 1) {
-                progress = 1;
-            }
 
-            //getting the portion of distance to animate
-            let shift = distance * progress;
-            elem.style.left = (initialLeftNum - shift) + "px";
+    useEffect(() => {
+        const scrollingText = refContainer.current;
+        initScrollingText(scrollingText, duration, isFinite);
+        /*eslint react-hooks/exhaustive-deps:0*/
+        //componentDidMount Effect
+    }, []);
 
-            if (progress < 1) {
-                requestAnimationFrame(measure);
-            } else {
-                /**
-                 * if fulfilled, then to replace the scrolling element to the initial not visible position
-                 * only in case of isInfinite === true
-                 */
-                if (isInfinite) {
-                    refreshTextPos();
-                    startTime = null;
-                    requestAnimationFrame(measure);
-                }
-            }
-        });
-
-    }
-
-    render() {
-        const { text } = this.props.data;
-
-        return (
-            text &&
-            <div id="scrolling-text-wrapper" >
-                <p
-                    className="scrolling-text"
-                    ref={ this.refContainer }
-                >
-                    { text }
-                </p>
-            </div>
-        );
-    }
-
-    componentDidMount() {
-        const { duration, isFinite } = this.props.data;
-        const scrollingText = this.refContainer.current;
-        this._initScrollingText(scrollingText, duration, isFinite);
-    }
+    return (
+        text.length &&
+        <div id="scrolling-text-wrapper" >
+            <p
+                className="scrolling-text"
+                ref={ refContainer }
+            >
+                { text }
+            </p>
+        </div>
+    );
 }
-
 
 ScrollingText.propTypes = {
     data: PropTypes.shape({
--- src/utils/services/userService.js
+++ src/utils/services/userService.js
@@ -56,6 +56,13 @@ export function handleData(data) {
     }
 }
 
+/**
+ *
+ * @param {string} path: url to data to be fetched
+ * @param {number} timeLimit: time limits for storing in localStorage (days)
+ * @param {string} extension: optional type of the data received from http request
+ * @returns {Promise} of the fetched data. Catch will be used outside
+ */
 export const getAndStore = async ( path, timeLimit=1, extension="json" ) => {
     const dataName = path.split("/").slice(-1)[0];
 
@@ -73,6 +80,57 @@ export const getAndStore = async ( path, timeLimit=1, extension="json" ) => {
         } )
 };
 
+/**
+ * @description it inits the scrolling text, which is wrapped in the parent at fixed position top of the window.
+ * @param {HTMLElement} elem: with text should have its own wrapper, parentElement in DOM,
+ * for elem is positioned to its wrapper
+ * @param {number} duration in milliseconds for scrolling all the text through the length of its wrapper
+ * @param {boolean} isInfinite: if true, the text starts scrolling infinitely
+ */
+export const initScrollingText = (elem, duration, isInfinite = false) => {
+    let startTime = null;
+    const textWrapper = elem.parentElement;
+    const distanceGap = 10;
+    const initialLeft = textWrapper.offsetWidth + distanceGap + "px";
+
+    elem.style.left = initialLeft;  //setting elem with text out of the visible bouderies of the parentElement
+
+    const initialLeftNum = parseInt(elem.style.left);
+    const distance = elem.offsetWidth + textWrapper.offsetWidth + distanceGap;
+
+    const refreshTextPos = () => {
+        elem.remove();
+        elem.style.left = initialLeft;
+        textWrapper.append(elem);
+    };
+
+    requestAnimationFrame(function measure(time) {
+        if (!startTime) {
+            startTime = time;
+        }
+        //getting the percentage of time, passed from startTime...
+        let progress = (time - startTime)/duration;   //from 0 to 1
+        if (progress > 1) {
+            progress = 1;
+        }
+
+        //getting the portion of distance to animate
+        let shift = distance * progress;
+        elem.style.left = (initialLeftNum - shift) + "px";
+
+        if (progress < 1) {
+            requestAnimationFrame(measure);
+        } else {
+            //if fulfilled, then to replace the scrolling element to the initial not visible position
+            //only in case of isInfinite === true
+            if (isInfinite) {
+                refreshTextPos();
+                startTime = null;
+                requestAnimationFrame(measure);
+            }
+        }
+    });
+};
 
 ///////////////// dev
 // eslint-disable-next-line no-unused-vars
