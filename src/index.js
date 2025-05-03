7fd60fdf608e4b228607ba722089505cac9ebd87
Rockets <vkomolov.ua@gmail.com>
2025-05-04 01:10:51 +0300

    to optimize reset and fonts imports

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./app";
+
+//STYLES
+import "./styles/reset.scss";
+import "./styles/fonts.scss";
 import "./styles/index.scss";
+
+
 import store from "./store";
 
 /*
